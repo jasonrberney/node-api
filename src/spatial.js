bc1d52e88507d76a885f6718980b41a19ac68f30
Jason Berney <jasonrberney@gmail.com>
2020-04-01 13:23:19 -0700

    change lat long to float

--- src/spatial.js
+++ src/spatial.js
@@ -8,12 +8,22 @@ getLocations = async () => {
         // pgClose(client);
         const locations = [
             {
-                name: 'portland',
-                coordinates: ['45.5051', '122.6750']
+                name: 'London',
+                coordinates: ['51.5074', '0.1278'],
+                latitude: 51.5074,
+                longitude: 0.1278
             },
             {
-                name: 'london',
-                coordinates: ['51.5074', '0.1278']
+                name: 'Portland',
+                coordinates: ['45.5051', '-122.6750'],
+                latitude: 45.5051,
+                longitude: -122.6750
+            },
+            {
+                name: 'Mexico City',
+                coordinates: ['19.4326', '-99.1332'],
+                latitude: 19.4326,
+                longitude: -99.1332
             }
         ];
         return {success : true , result : locations };
